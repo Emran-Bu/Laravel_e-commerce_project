34a856db761ab2b33b3a00f9252c5e42577ace20
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-10-02 11:02:51 +0600

    header page created

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,2 +1,26 @@
 @extends('master')
 @section('content')
+<div class="conainer">
+    <div class="row">
+        <div class="col-6 offset-3">
+            <form action="" method="get">
+                <div class="form-group">
+                    <label for="lemail">Email Address</label>
+                    <input class="form-control" type="email" name="lemail" id="lemail">
+                    <small class="form-text text-muted">we'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="lpassword">Password</label>
+                    <input class="form-control" type="password" name="lpassword" id="lpassword">
+                </div>
+                <div class="form-group mt-2 form-check">
+                    <input class="form-check-input" type="checkbox" name="lcheck" id="lcheck">
+                    <label class="form-check-label" for="lcheck">check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary btn-sm mt-2">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+<button class="btn btn-danger">click me</button>
+@endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,21 +8,22 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     {{-- custom css Link--}}
     <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
-    <title>Document</title>
+    <title>E-Commerce_Project</title>
 </head>
 <body>
-    <h3 class="text-warning">master page</h3>
-    <button class="btn btn-danger">click me</button>
-    <h1>java</h1>
+
+    {{ View::make('header') }}
+
+    @yield('content')
+
+    {{ View::make('footer') }}
 
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 
     <script>
         $(document).ready(function() {
-            $('button').click(function() {
-                alert('all set');
-            })
+            
         })
     </script>
 </body>
